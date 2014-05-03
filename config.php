58a216e88b89f77c4d05ff08a79fd98f692d8ee4
Skylar Kelty <skylarkelty@gmail.com>
2014-05-03 18:10:52 +0100

    Add setup.php

--- config.php
+++ config.php
@@ -9,4 +9,7 @@ $CFG = new \stdClass();
 $CFG->dirroot = dirname(__FILE__);
 $CFG->libdir = $CFG->dirroot . "/lib";
 
+// Special file, created at deploy time. Contains API key.
+require_once($CFG->dirroot . "/data.php");
+
 require_once($CFG->libdir . "/autoloader.php");
