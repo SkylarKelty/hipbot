71f9406fec5b08e0e10d680592422dd7326f722b
Skylar Kelty <skylarkelty@gmail.com>
2014-05-03 23:07:07 +0100

    Add example data

---
+++ data-dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$CFG->wwwroot = "http://YOUR-WEBSITE-HERE";
+
+$CFG->oauth_id = "OAUTH ID HERE";
+$CFG->oauth_secret = "OAUTH SECRET HERE";
+$CFG->roomid = "ROOM ID HERE";
+
+$CFG->db_uri = "mysql:dbname=hipbot;host=127.0.0.1";
+$CFG->db_user = "hipbot";
+$CFG->db_pass = "CHANGEME";
--- install.php
+++ install.php
@@ -13,7 +13,7 @@ print "Registering webhook";
 
 $url = "https://api.hipchat.com/v2/room/{$CFG->roomid}/webhook";
 $body = json_encode(array(
-	"url" => "http://{$CFG->wwwroot}/hooks/message.php",
+	"url" => "{$CFG->wwwroot}/hooks/message.php",
 	"pattern" => "",
 	"event" => "room_message",
 	"name" => "hipbot_message_hook"
